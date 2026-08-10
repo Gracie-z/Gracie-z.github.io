113e97638e49d3ed416829aae08a114353fb3aa5
Gracie-z <0lespetitsanimaux0@gmail.com>
2026-08-10 11:37:36 -0700

    Refresh CS224N lecture companion

--- docs/mle/router.js
+++ docs/mle/router.js
@@ -1,6 +1,6 @@
 (() => {
   const workerOrigin = "https://gracie-quant-archive.gracie-quant-archive.workers.dev";
-  const workerVersion = "20260810-11";
+  const workerVersion = "20260810-12";
   const publicBase = "/mle";
   const frame = document.querySelector("#mleArchiveFrame");
 
